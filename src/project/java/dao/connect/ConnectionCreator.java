e731ade88bc720bc20232003f7043047332a1528
Pavel <pavel3423@gmail.com>
2018-03-19 08:11:02 +0300

    fix pool DataSource

--- src/project/java/dao/connect/ConnectionCreator.java
+++ src/project/java/dao/connect/ConnectionCreator.java
@@ -1,33 +1,45 @@
 package project.java.dao.connect;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionCreator {
-    private static final String URL_DB = "jdbc:mysql://127.0.0.1:2016/?useUnicode=true&characterEncoding=UTF-8";
-    private static final String USER_DB = "root";
-    private static final String PASSWORD_DB = "";
-    private static Connection connection;
+    //    private static final String URL_DB = "jdbc:mysql://127.0.0.1:2016/?useUnicode=true&characterEncoding=UTF-8";
+//    private static final String USER_DB = "root";
+//    private static final String PASSWORD_DB = "";
+//    private static Connection connection;
+    private static DataSource dataSource;
 
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(URL_DB, USER_DB, PASSWORD_DB);
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return connection;
-
+//    public static Connection getConnection() {
 //        try {
-//            InitialContext initialContext = new InitialContext();
-//            DataSource dataSource = (DataSource) initialContext.lookup("java:/comp/env/jdbc/my_sql_kozlov");
-//            return dataSource.getConnection();
-//        } catch (NamingException | SQLException e) {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            if (connection == null || connection.isClosed()) {
+//                connection = DriverManager.getConnection(URL_DB, USER_DB, PASSWORD_DB);
+//            }
+//        } catch (SQLException | ClassNotFoundException e) {
 //            e.printStackTrace();
 //        }
-//        return null;
+//        return connection;
+
+    private static DataSource getDataSource() throws NamingException {
+        if (dataSource == null) {
+            InitialContext initialContext = new InitialContext();
+            dataSource = (DataSource) initialContext.lookup("java:/comp/env/jdbc/my_sql_akhmelev");
+        }
+        return dataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return getDataSource().getConnection();
+        } catch (SQLException e) {
+            throw e;
+        } catch (NamingException e) {
+            throw new SQLException(e);
+        }
     }
 }
